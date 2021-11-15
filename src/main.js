81a1228d8fcd685dc908805631a49b4307c1472a
Christoph Samuel <chsamuel@gmx.at>
2021-11-15 22:26:35 +0100

    added "vue-material"- and "axios"-package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ require('aframe')
 require('ar.js')
 import Vue from 'vue'
 import App from './App.vue'
+import options from '@/config'
+import '@/main.css'
 
 Vue.config.productionTip = false
 Vue.config.ignoredElements = [
@@ -16,5 +18,6 @@ Vue.config.ignoredElements = [
 ]
 
 new Vue({
+    ...options(Vue),
     render: h => h(App),
 }).$mount('#app')
