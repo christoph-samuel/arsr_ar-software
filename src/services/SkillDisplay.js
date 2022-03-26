0bfa003b549e777d9b8c9b25cc4d889e5d013cc9
Christoph Samuel <chsamuel@gmx.at>
2022-03-26 22:58:49 +0100

    changed from SkillDisplay Dev-Server to Live-Server

--- src/services/SkillDisplay.js
+++ src/services/SkillDisplay.js
@@ -2,44 +2,41 @@ import axios from 'axios'
 import {sha256} from 'js-sha256'
 
 export class SkillDisplay {
-    apiKey = "5de7d6b5-e63e-4d47-b3c8-f365f997d1d3"
+    apiKey = "5a0a96ad-ff57-4cf0-8281-8486472aaa34"
+    // apiKey = "5de7d6b5-e63e-4d47-b3c8-f365f997d1d3"
 
     getSkillSet(id) {
-        // TODO: let url = 'https://www.skilldisplay.eu/api/v1/skillset/' + id + '?includeFullSkills'
-        let url = 'https://dev.skilldisplay.eu/api/v1/skillset/' + id + '?includeFullSkills'
+        let url = 'https://www.skilldisplay.eu/api/v1/skillset/' + id + '?includeFullSkills'
+        // let url = 'https://dev.skilldisplay.eu/api/v1/skillset/' + id + '?includeFullSkills'
         return axios.get(url, {headers: {"x-api-key": this.apiKey}}).then(response => {
             return response.data
         });
     }
 
     getSkill(id) {
-        // TODO: let url = 'https://www.skilldisplay.eu/api/v1/skill/' + id
-        let url = 'https://dev.skilldisplay.eu/api/v1/skill/' + id
+        let url = 'https://www.skilldisplay.eu/api/v1/skill/' + id
+        // let url = 'https://dev.skilldisplay.eu/api/v1/skill/' + id
         return axios.get(url, {headers: {"x-api-key": this.apiKey}}).then(response => {
             return response.data
         });
     }
 
     setVerification(skillSetID, skillId, level) {
-        // TODO: let url = 'https://www.skilldisplay.eu/api/v1/verification/create'
-        let url = 'https://dev.skilldisplay.eu/api/v1/verification/create'
-        let secret = 'ba19cee0-1c91-4195-906d-491e04a2dd11';
+        let url = 'https://www.skilldisplay.eu/api/v1/verification/create'
+        // let url = 'https://dev.skilldisplay.eu/api/v1/verification/create'
+        let secret = '1500e6ea-41fa-41cd-bcdb-fd4e9434e2e9';
+        // let secret = 'ba19cee0-1c91-4195-906d-491e04a2dd11';
         let signature = level === "self" ? 'sdself' : secret
         let requestData = {
             'SkillSetId': skillSetID,
             'SkillId': skillId,
             'Level': level,
-            'VerifierId': 111,
+            'VerifierId': 136,
+            // 'VerifierId': 111,
             'Username': 'diplomarbeit.arsr@gmail.com',
             'AutoConfirm': false,
             'Signature': ''
         }
-        // console.log("Type:", typeof requestData)
-        // console.log("RequestData:", requestData)
-        // console.log("Secret:", secret)
-        // console.log("Level:", level)
-        // console.log("let Signature:", signature)
-        // console.log("String to hash:", JSON.stringify(requestData) + signature)
         requestData['Signature'] = sha256(JSON.stringify(requestData) + signature);
         console.log("RequestData:", requestData)
         console.log("Signature:", requestData['Signature'])
